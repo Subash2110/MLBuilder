f8e195c7d96e622948459718c326596a3ae45990
Subash S <subash211002s@gmail.com>
2026-06-12 13:00:57 +0530

    changes

---
+++ .angular/cache/20.3.26/ml-builder/vite/deps/@angular_cdk_scrolling.js
@@ -0,0 +1,50 @@
+import {
+  CdkFixedSizeVirtualScroll,
+  CdkScrollable,
+  CdkScrollableModule,
+  CdkVirtualForOf,
+  CdkVirtualScrollViewport,
+  CdkVirtualScrollable,
+  CdkVirtualScrollableElement,
+  CdkVirtualScrollableWindow,
+  DEFAULT_RESIZE_TIME,
+  DEFAULT_SCROLL_TIME,
+  FixedSizeVirtualScrollStrategy,
+  ScrollDispatcher,
+  ScrollingModule,
+  VIRTUAL_SCROLLABLE,
+  VIRTUAL_SCROLL_STRATEGY,
+  ViewportRuler,
+  _fixedSizeVirtualScrollStrategyFactory
+} from "./chunk-AXON5RKD.js";
+import "./chunk-Q4SBNR36.js";
+import "./chunk-5EG33CFQ.js";
+import {
+  Dir
+} from "./chunk-FC32BE5F.js";
+import "./chunk-TVI55ZVO.js";
+import "./chunk-WS7XDFFJ.js";
+import "./chunk-UI7BW4CF.js";
+import "./chunk-ONCGLZD2.js";
+import "./chunk-RSS3ODKE.js";
+import "./chunk-WDMUDEB6.js";
+export {
+  CdkFixedSizeVirtualScroll,
+  CdkScrollable,
+  CdkScrollableModule,
+  CdkVirtualForOf,
+  CdkVirtualScrollViewport,
+  CdkVirtualScrollable,
+  CdkVirtualScrollableElement,
+  CdkVirtualScrollableWindow,
+  DEFAULT_RESIZE_TIME,
+  DEFAULT_SCROLL_TIME,
+  FixedSizeVirtualScrollStrategy,
+  ScrollDispatcher,
+  ScrollingModule,
+  VIRTUAL_SCROLLABLE,
+  VIRTUAL_SCROLL_STRATEGY,
+  ViewportRuler,
+  _fixedSizeVirtualScrollStrategyFactory,
+  Dir as ɵɵDir
+};
---
+++ .angular/cache/20.3.26/ml-builder/vite/deps/@angular_material_form-field.js
@@ -0,0 +1,94 @@
+import {
+  MatFormFieldModule
+} from "./chunk-IRSKWJCN.js";
+import {
+  MAT_ERROR,
+  MAT_FORM_FIELD,
+  MAT_FORM_FIELD_DEFAULT_OPTIONS,
+  MAT_PREFIX,
+  MAT_SUFFIX,
+  MatError,
+  MatFormField,
+  MatFormFieldControl,
+  MatHint,
+  MatLabel,
+  MatPrefix,
+  MatSuffix,
+  getMatFormFieldDuplicatedHintError,
+  getMatFormFieldMissingControlError,
+  getMatFormFieldPlaceholderConflictError
+} from "./chunk-XCU5RJ3R.js";
+import "./chunk-3LLOHGS2.js";
+import "./chunk-X66UBXYL.js";
+import "./chunk-VENV3F3G.js";
+import "./chunk-GWFLKVBH.js";
+import "./chunk-5EG33CFQ.js";
+import "./chunk-TBUIKOA5.js";
+import "./chunk-Y2NQ33XO.js";
+import "./chunk-FC32BE5F.js";
+import "./chunk-SGYGGF4K.js";
+import "./chunk-TVI55ZVO.js";
+import "./chunk-WS7XDFFJ.js";
+import "./chunk-UI7BW4CF.js";
+import "./chunk-ONCGLZD2.js";
+import "./chunk-RSS3ODKE.js";
+import "./chunk-WDMUDEB6.js";
+
+// node_modules/@angular/material/fesm2022/form-field.mjs
+var matFormFieldAnimations = {
+  // Represents:
+  // trigger('transitionMessages', [
+  //   // TODO(mmalerba): Use angular animations for label animation as well.
+  //   state('enter', style({opacity: 1, transform: 'translateY(0%)'})),
+  //   transition('void => enter', [
+  //     style({opacity: 0, transform: 'translateY(-5px)'}),
+  //     animate('300ms cubic-bezier(0.55, 0, 0.55, 0.2)'),
+  //   ]),
+  // ])
+  /** Animation that transitions the form field's error and hint messages. */
+  transitionMessages: {
+    type: 7,
+    name: "transitionMessages",
+    definitions: [
+      {
+        type: 0,
+        name: "enter",
+        styles: {
+          type: 6,
+          styles: { opacity: 1, transform: "translateY(0%)" },
+          offset: null
+        }
+      },
+      {
+        type: 1,
+        expr: "void => enter",
+        animation: [
+          { type: 6, styles: { opacity: 0, transform: "translateY(-5px)" }, offset: null },
+          { type: 4, styles: null, timings: "300ms cubic-bezier(0.55, 0, 0.55, 0.2)" }
+        ],
+        options: null
+      }
+    ],
+    options: {}
+  }
+};
+export {
+  MAT_ERROR,
+  MAT_FORM_FIELD,
+  MAT_FORM_FIELD_DEFAULT_OPTIONS,
+  MAT_PREFIX,
+  MAT_SUFFIX,
+  MatError,
+  MatFormField,
+  MatFormFieldControl,
+  MatFormFieldModule,
+  MatHint,
+  MatLabel,
+  MatPrefix,
+  MatSuffix,
+  getMatFormFieldDuplicatedHintError,
+  getMatFormFieldMissingControlError,
+  getMatFormFieldPlaceholderConflictError,
+  matFormFieldAnimations
+};
+//# sourceMappingURL=@angular_material_form-field.js.map
